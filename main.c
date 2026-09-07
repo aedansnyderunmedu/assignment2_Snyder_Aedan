e2a6e9d47f8586f42e2b6187f5e6f098d44f65ef
InvariablyGib <aedansnyder@gmail.com>
2026-09-07 11:51:10 -0600

    Pushing wouldn't work for some reason. Going to push each file again 1 by 1.

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "statistics.h"
+#include "output_array.h"
 
 void insideError(void)
 {
@@ -27,7 +28,8 @@ int fill_Array(int *array, int n) //array fill function, takes n for size/loop a
 
 int main()
 {
-	int n_Max = 500;
+	int rowCount = 6;//fixed table row size for printing
+	int n_Max = 500;//VLAs live on the stack; don't want arrays to be too large.
 	int n;//array 1 parameter; user input.
 	
 	printf("Please enter size of table 1:\n");
@@ -37,7 +39,7 @@ int main()
 		return 1; //rejection/exit
 	}
 	int table1[n];
-		int n1 = n;
+		int n1 = n; //saving historical values of n for passing into generic stats function size parameter. 
 	fill_Array(table1, n);
 
 	printf("Please enter size of table 2:\n");
@@ -59,8 +61,23 @@ int main()
 	int table3[n];
 		int n3 = n;
 	fill_Array(table3, n);
-	
-	printf("Table 1 metrics:\nSum: %d\nAverage: %lf\nStandard Deviation (sample): %lf\n", sum_Array(table1, n1), avg_Array(table1, n1), stdev_Array(table1, n1));
-
+	//=====Table 1 Logic=====>
+	printf("TABLE 1:\n");
+	output_Array(table1, n1, rowCount);
+	int max1 = max_Array(table1, n1), min1 = min_Array(table1, n1), abs1 = abs_Array(max1, min1);
+	printf("Table 1 metrics:\nSum: %d\nAverage: %lf\nStandard Deviation(sample): %lf\nMax value: %d\nMin value: %d\nTotal range: %d\n", sum_Array(table1, n1), avg_Array(table1, n1), stdev_Array(table1, n1), max1, min1, abs1);
+	printf("==============================\n");
+	//=====Table 2 Logic=====>
+	printf("TABLE 2:\n");
+	output_Array(table2, n2, rowCount);
+	int max2 = max_Array(table2, n2), min2 = min_Array(table2, n2), abs2 = abs_Array(max2, min2);
+	printf("Table 2 metrics:\nSum: %d\nAverage: %lf\nStandard Deviation(sample): %lf\nMax value: %d\nMin value: %d\nTotal range: %d\n", sum_Array(table2, n2), avg_Array(table2, n2), stdev_Array(table2, n2), max2, min2, abs2);
+	printf("==============================\n");	
+	//=====Table 3 Logic=====>
+	printf("TABLE 3:\n");
+	output_Array(table3, n3, rowCount);
+	int max3 = max_Array(table3, n3), min3 = min_Array(table3, n3), abs3 = abs_Array(max3, min3);
+	printf("Table 3 metrics:\nSum: %d\nAverage: %lf\nStandard Deviation(sample): %lf\nMax value: %d\nMin value: %d\nTotal range: %d\n", sum_Array(table3, n3), avg_Array(table3, n3), stdev_Array(table3, n3), max3, min3, abs3);
+	printf("==============================\n");
 	return 0;
 }
