b6bdd13b69881e9d8d9ea767da0f003298074ec5
InvariablyGib <aedansnyder@gmail.com>
2026-09-07 08:30:32 -0600

    stats.h was updated and commited but never readded, so the changes didn't push. not that were really any changes to push, it's a simple header.

---
+++ statistics.c
@@ -0,0 +1,6 @@
+#include <math.h>
+#include "statistics.h" //quotes to specify THIS directory. stats.h is my custom header, not included in the main c toolchain. 
+
+double stdev_Array(*array, n)
+{
+
---
+++ statistics.h
@@ -0,0 +1,10 @@
+#ifndef STATISTICS_H //if undefined
+#define STATISTICS_H //define. INCLUDE guards to prevent using my stats.h more than once
+
+double avg_Array(const int *array, int n); //All basic prototypes. same implementation as before in assignment 1. 
+double stdev_Array(const int *array, int n); //
+int sum_Array(const int *array, int n); //
+int max_Array(const int *array, int n); //
+int min_Array(const int *array, int n); //
+
+#endif //close header
